8a66ea19dc3489fde42c1f256586ab79ac9a607a
Vincent Tourraine <me@vtourraine.net>
2015-09-22 22:06:10 +0200

    Update project configuration

--- Tests/Pods/Target Support Files/Pods-AcknowExample-AcknowList/Pods-AcknowExample-AcknowList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AcknowExample_AcknowList : NSObject
-@end
-@implementation PodsDummy_Pods_AcknowExample_AcknowList
-@end
--- Tests/Pods/Target Support Files/Pods-AcknowExampleTests-AcknowList/Pods-AcknowExampleTests-AcknowList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AcknowExampleTests_AcknowList : NSObject
-@end
-@implementation PodsDummy_Pods_AcknowExampleTests_AcknowList
-@end
