84f653eb9f31703f6fd6f0dda5f8ef459bb5b194
James White <james@behere.be>
2016-03-30 19:58:27 +0100

    Purge CocoaPods from the example project.

---
+++ Example/AcknowExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AcknowExample
+//
+//  Created by Vincent Tourraine on 15/08/15.
+//  Copyright (c) 2015-2016 VTourraine. All rights reserved.
+//
+
+import UIKit
+
+import AcknowList
+
+class ViewController: UIViewController {
+
+    @IBAction func pushAcknowList(sender: AnyObject) {
+        let viewController = AcknowListViewController()
+
+        if let navigationController = self.navigationController {
+            navigationController.pushViewController(viewController, animated: true)
+        }
+    }
+}
