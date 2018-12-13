0194d87ef438b1c7f2af47f4c168ca849b817cbb
Maxim V. Potapov <vopatop.skam@gmail.com>
2018-12-13 14:52:40 +0300

    inherit controller push settings

--- Source/AcknowListViewController.swift
+++ Source/AcknowListViewController.swift
@@ -452,6 +452,7 @@ open class AcknowListViewController: UITableViewController {
         let acknowledgement = acknowledgements[(indexPath as NSIndexPath).row] as Acknow?,
         let navigationController = self.navigationController {
                 let viewController = AcknowViewController(acknowledgement: acknowledgement)
+                viewController.hidesBottomBarWhenPushed = hidesBottomBarWhenPushed
                 navigationController.pushViewController(viewController, animated: true)
         }
     }
