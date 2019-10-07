f80bcdc524b477b07a1cc6c9a428f25e069e9602
Vincent Tourraine <me@vtourraine.net>
2019-10-07 10:58:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AcknowList",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v8), .tvOS(.v9)
     ],
     products: [
         .library(name: "AcknowList", targets: ["AcknowList"])
