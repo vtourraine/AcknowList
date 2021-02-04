94b40167b161b78a7ccf266c7fe989a3b55c8678
Vincent Tourraine <me@vtourraine.net>
2021-02-04 09:31:41 +0100

    Update CocoaPods example project

--- Examples/AcknowExampleCocoaPods/AcknowExampleCocoaPods/ViewController.swift
+++ Examples/AcknowExampleCocoaPods/AcknowExampleCocoaPods/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
 
     @IBAction func presentAcknowledgements(_ sender: UIButton) {
         let vc = AcknowListViewController()
-        vc.acknowledgements = [Acknow(title: "T1", text: "abc"),
-                               Acknow(title: "T2", text: "abc"),
-                               Acknow(title: "T3", text: "abc")]
         let navigationController = UINavigationController(rootViewController: vc)
         present(navigationController, animated: true, completion: nil)
     }
