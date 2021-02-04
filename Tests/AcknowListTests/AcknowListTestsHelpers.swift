7becac651ce8c18b0812fe5a4f27c937a2b64fc2
Vincent Tourraine <me@vtourraine.net>
2021-02-04 17:07:51 +0100

    Support SPM test target

---
+++ Tests/AcknowListTests/AcknowListTestsHelpers.swift
@@ -0,0 +1,24 @@
+//
+//  AcknowListTestsHelpers.swift
+//  AcknowExample
+//
+//  Created by Vincent Tourraine on 04/12/
+//  Copyright © 2015-2021 Vincent Tourraine. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+
+    func resourcesBundle() -> Bundle {
+        let bundle = Bundle(for: AcknowParserTests.self)
+
+        let spmTestBundlePath = bundle.bundlePath.appending("/../AcknowList_AcknowListTests.bundle")
+        if let spmTestBundle = Bundle(path: spmTestBundlePath) {
+            return spmTestBundle
+        }
+        else {
+            return bundle
+        }
+    }
+}
