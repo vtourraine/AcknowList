d3e5dcf7d4cf798db5c3e75c1b27463b27fb1134
Vincent Tourraine <me@vtourraine.net>
2022-01-12 18:36:02 +0100

    Bump copyright year

--- Tests/AcknowListTests/AcknowLocalizationTests.swift
+++ Tests/AcknowListTests/AcknowLocalizationTests.swift
@@ -3,7 +3,7 @@
 //  AcknowExample
 //
 //  Created by Vincent Tourraine on 1/05/16.
-//  Copyright © 2015-2021 Vincent Tourraine. All rights reserved.
+//  Copyright © 2015-2022 Vincent Tourraine. All rights reserved.
 //
 
 import XCTest
